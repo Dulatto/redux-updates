6e95c74b3d307308fe86e29bd7ddc94707ce20cc
Dulatto <dulat.omarov@gmail.com>
2022-04-24 14:14:40 -0400

    store

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const redux = require('redux');
-const createStore = redux.createStore;
+const createStore = redux.createStore
 const bindActionCreators = redux.bindActionCreators
 
 const CAKE_ORDERED = 'CAKE_ORDERED'
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED'
+const ICECREAM_ORDERED = 'ICECREAM_ORDERED'
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED'
 
 function orderCake() {
     return {
         type: CAKE_ORDERED,
         payload: 1,
+    }
+}
+function orderIceCream(qty = 1) {
+    return {
+        type: ICECREAM_ORDERED,
+        payload: qty
     }
 }
 
@@ -18,9 +26,16 @@ function restockCake(qty = 1) {
         payload: qty,
     }
 }
+function restockIceCream(qty = 1) {
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
+    }
+}
 
 const initialState = {
     numOfCakes: 10,
+    numOfIceCreams: 20
 }
 
 //(previousState, action) => newState
@@ -37,6 +52,16 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 numOfCakes: state.numOfCakes + action.payload,
             }
+        case ICECREAM_ORDERED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams - 1
+            }
+        case ICECREAM_RESTOCKED:
+            return {
+                ...state,
+                numOfIceCreams: state.numOfIceCreams + action.payload,
+            }
         default:
             return state
     }
@@ -52,10 +77,13 @@ const unsubscribe = store.subscribe(() => console.log('update state', store.getS
 // store.dispatch(orderCake())
 // store.dispatch(restockCake(3))
 
-const actions = bindActionCreators({ orderCake, restockCake }, store.dispatch)
+const actions = bindActionCreators({ orderCake, restockCake, orderIceCream, restockIceCream }, store.dispatch)
 actions.orderCake()
 actions.orderCake()
 actions.orderCake()
 actions.restockCake(3)
+actions.orderIceCream()
+actions.orderIceCream()
+actions.restockIceCream(2)
 
 unsubscribe()
