80f4a7503cd803c2b2249e5bd409f8bac4b6bb12
Dulatto <dulat.omarov@gmail.com>
2022-04-25 21:19:31 -0400

    immer

---
+++ nested-state.js
@@ -0,0 +1,32 @@
+const initialState = {
+    name: 'Vishwas',
+    address: {
+        street: '123 Main st',
+        city: 'Montreal',
+        state: 'MA',
+    }
+}
+const STREET_UPDATED = 'STREET_UPDATED'
+const updateStreet = (street) => {
+    return {
+        type: STREET_UPDATED,
+        payload: street,
+    }
+}
+
+const reducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'STREET_UPDATED':
+            return {
+                ...state,
+                address: {
+                    ...state.address,
+                    street: action.payload
+                },
+            }
+        default: {
+            return state
+        }
+    }
+}
